c73263c933c5292e05ae859dec0a1d1e1632911a
Jose A. Campos <jjoseccampos@gmail.com>
2017-04-03 11:29:33 -0400

    add class to handle database

--- tiydatabase.rb
+++ tiydatabase.rb
@@ -2,26 +2,66 @@ require 'sinatra'
 require 'pg'
 require 'sinatra/reloader' if development?
 
+class Employee
+  attr_reader "id", "name", "phone", "address", "position" , "salary", "slack", "github"
+
+  def initialize(employee)
+    @id = employee["id"]
+    @name = employee["name"]
+    @phone = employee["phone"]
+    @address = employee["address"]
+    @position = employee["position"]
+    @salary = employee["salary"]
+    @slack = employee["slack"]
+    @github = employee["github"]
+  end
+
+  def self.create(params)
+    database = PG.connect(dbname: "tiy-database")
+    name     = params["name"]
+    phone    = params["phone"]
+    address  = params["address"]
+    position = params["position"]
+    salary   = params["salary"]
+    github   = params["github"]
+    slack    = params["slack"]
+
+    database.exec("insert into employees (name, phone, address, position, salary, github, slack) values($1, $2, $3, $4, $5, $6, $7)", [name, phone, address, position, salary, github, slack])
+  end
+
+  def self.all
+    database = PG.connect(dbname: "tiy-database")
+
+    return database.exec("select * from employees").map { |employee| Employee.new(employee) }
+  end
+
+  def self.find(id)
+    database = PG.connect(dbname: "tiy-database")
+
+    employees = database.exec("select * from employees where id =$1", [id]).map { |employee| Employee.new(employee) }
+
+    return employees.first
+  end
+
+  def self.search(text)
+    database = PG.connect(dbname: "tiy-database")
+
+    return database.exec("select * from employees where name like $1 or github=$2 or slack=$2", ["%#{text}%", text]).map { |employee| Employee.new(employee) }
+  end
+end
+
 get '/' do
   erb :home
 end
 
 get '/employees' do
-  database = PG.connect(dbname: "tiy-database")
-
-  @employees = database.exec ("select * from employees")
+  @employees = Employee.all
 
   erb :employees
 end
 
 get '/employee_show' do
-  database = PG.connect(dbname: "tiy-database")
-
-  id = params["id"]
-
-  employees = database.exec("select * from employees where id =$1", [id])
-
-  @employee = employees.first
+  @employee = Employee.find(params["id"])
   if @employee
     erb :employee_show
   else
@@ -34,28 +74,14 @@ get '/new' do
 end
 
 get '/employees_new' do
-  database = PG.connect(dbname: "tiy-database")
-
-  name     = params["name"]
-  phone    = params["phone"]
-  address  = params["address"]
-  position = params["position"]
-  salary   = params["salary"]
-  github   = params["github"]
-  slack    = params["slack"]
-
-  database.exec("insert into employees (name, phone, address, position, salary, github, slack) values($1, $2, $3, $4, $5, $6, $7)", [name, phone, address, position, salary, github, slack])
+  Employee.create(params)
 
   redirect('/')
 end
 
 get '/searched' do
-  database = PG.connect(dbname: "tiy-database")
-
   search = params["search"]
-
-  @employees = database.exec("select * from employees where name like $1 or github=$2 or slack=$3", ["%#{search}%", search, search])
-
+  @employees = Employee.search(search)
   erb :searched
 end
 
